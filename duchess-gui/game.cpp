c549686fb4b7d5fde56f1734434c37c8a96defa8
Pynikleois <branko.pekez.peka@gmail.com>
2023-01-20 01:28:40 +0100

    Add basics of mouse dragging.

--- duchess-gui/game.cpp
+++ duchess-gui/game.cpp
@@ -1,43 +1,47 @@
 #include "game.h"
 
+#include <iostream>
+
 const sf::Time Game::kTimePerFrame = sf::seconds(1.f / 60.f);
 
 Game::Game() : window_{sf::VideoMode(900, 900), "Duchess-GUI", sf::Style::Close}, board_{720} {
   LoadTextures();
   //board_.SetTexture(textures_.Get("chessboard"));
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kRook,   0, 0, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kRook, 0, 0, textures_);
   board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kKnight, 1, 0, textures_);
   board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kBishop, 2, 0, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kQueen,  3, 0, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kKing,   4, 0, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kQueen, 3, 0, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kKing, 4, 0, textures_);
   board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kBishop, 5, 0, textures_);
   board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kKnight, 6, 0, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kRook,   7, 0, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   0, 1, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   1, 1, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   2, 1, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   3, 1, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   4, 1, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   5, 1, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   6, 1, textures_);
-  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn,   7, 1, textures_);
-  
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   0, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   1, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   2, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   3, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   4, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   5, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   6, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn,   7, 6, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kRook,   0, 7, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kRook, 7, 0, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 0, 1, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 1, 1, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 2, 1, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 3, 1, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 4, 1, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 5, 1, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 6, 1, textures_);
+  board_.NewPiece(Chessman::Color::kBlack, Chessman::Type::kPawn, 7, 1, textures_);
+
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 0, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 1, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 2, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 3, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 4, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 5, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 6, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kPawn, 7, 6, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kRook, 0, 7, textures_);
   board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kKnight, 1, 7, textures_);
   board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kBishop, 2, 7, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kQueen,  3, 7, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kKing,   4, 7, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kQueen, 3, 7, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kKing, 4, 7, textures_);
   board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kBishop, 5, 7, textures_);
   board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kKnight, 6, 7, textures_);
-  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kRook,   7, 7, textures_);
+  board_.NewPiece(Chessman::Color::kWhite, Chessman::Type::kRook, 7, 7, textures_);
+
+  board_.SetPosition({90, 90});
 }
 
 void Game::Run() {
@@ -87,12 +91,33 @@ void Game::LoadTextures() {
                  sf::IntRect(5 * d, d, d, d));
 }
 
+bool press = false;
+bool drag = false;
+
 void Game::ProcessInput() {
   sf::Event event;
   while (window_.pollEvent(event)) {
     switch (event.type) {
       case sf::Event::Closed:
         window_.close();
+        break;
+      case sf::Event::MouseButtonPressed:
+        if (event.mouseButton.button == sf::Mouse::Left) {
+          press = true;
+          std::cout << event.mouseButton.x << ", " << event.mouseButton.y << std::endl;
+        }
+        break;
+      case sf::Event::MouseMoved:
+        if (press) {
+          drag = true;
+          std::cout << event.mouseMove.x << ", " << event.mouseMove.y << std::endl;
+        }
+        break;
+      case sf::Event::MouseButtonReleased:
+        if (event.mouseButton.button == sf::Mouse::Left) {
+          drag = false;
+          press = false;
+        }
         break;
     }
   }
@@ -102,7 +127,6 @@ void Game::Update(sf::Time elapsedTime) {}
 
 void Game::Render() {
   window_.clear(sf::Color::White);
-  board_.SetPosition({90, 90});
   window_.draw(board_);
   window_.display();
 }
